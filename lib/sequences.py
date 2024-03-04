31355e700f61016e90c2fe406fe3b685bde254ac
Marvin <okongomarvin971@gmail.com>
2024-03-04 08:38:51 +0300

    Completed Assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    seq = [0, 1]
+    
+    while len(seq) < length:
+        next_num = seq[-1] + seq[-2]
+        seq.append(next_num)
+    
+    print(seq[:length])
